a2248a94761f4c17beb1ed65f1ea5212b09e129d
Alex Beloi <beloi.alex@gmail.com>
2018-07-03 13:36:52 -0700

    train: add train/eval prefix to summaries

--- tacotron/models/estimator.py
+++ tacotron/models/estimator.py
@@ -3,18 +3,26 @@ import tacotron.models.tacotron as tacotron
 
 
 def create_summaries(model, hparams, is_training=False, is_evaluating=True):
-    tf.summary.histogram('mel_outputs', model.mel_outputs)
+    if is_training:
+        prefix = 'train/'
+    elif is_evaluating:
+        prefix = 'eval/'
+    else:
+        prefix = ''
+
+    tf.summary.histogram(prefix + 'mel_outputs', model.mel_outputs)
     if is_training or is_evaluating:
-        tf.summary.histogram('mel_targets', model.mel_targets)
-        tf.summary.scalar('before_loss', model.before_loss)
-        tf.summary.scalar('after_loss', model.after_loss)
-        tf.summary.scalar('regularization_loss', model.regularization_loss)
-        tf.summary.scalar('stop_token_loss', model.stop_token_loss)
-        tf.summary.scalar('loss', model.loss)
+        tf.summary.histogram(prefix + 'mel_targets', model.mel_targets)
+        tf.summary.scalar(prefix + 'before_loss', model.before_loss)
+        tf.summary.scalar(prefix + 'after_loss', model.after_loss)
+        tf.summary.scalar(prefix + 'regularization_loss',
+                          model.regularization_loss)
+        tf.summary.scalar(prefix + 'stop_token_loss', model.stop_token_loss)
+        tf.summary.scalar(prefix + 'loss', model.loss)
         if hparams.tacotron_teacher_forcing_mode == 'scheduled':
-            tf.summary.scalar('teacher_forcing_ratio', model.ratio)
+            tf.summary.scalar(prefix + 'teacher_forcing_ratio', model.ratio)
         if is_training:
-            tf.summary.scalar('learning_rate', model.learning_rate)
+            tf.summary.scalar(prefix + 'learning_rate', model.learning_rate)
         alignments = model.alignments
         images = []
         for i in range(hparams.tacotron_batch_size):
@@ -24,7 +32,7 @@ def create_summaries(model, hparams, is_training=False, is_evaluating=True):
             images.append(image_resized)
         images_stacked = tf.stack(images)
 
-        tf.summary.image('alignments',
+        tf.summary.image(prefix + 'alignments',
                          images_stacked,
                          family='Images')
 
